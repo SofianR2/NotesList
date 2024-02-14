303b1341fd1eed7a8459ec0a6dbdc4a2b408b8df
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2024-02-14 01:48:07 -0600

    title change

--- ui/src/components/AddTaskForm.js
+++ ui/src/components/AddTaskForm.js
@@ -26,7 +26,7 @@ export const AddTaskForm = ({ fetchTasks }) => {
   return (
     <div>
       <Typography align="center" variant="h2" paddingTop={2} paddingBottom={2}>
-        My Task List
+        Notes
       </Typography>
       <div className="addTaskForm">
         <TextField
